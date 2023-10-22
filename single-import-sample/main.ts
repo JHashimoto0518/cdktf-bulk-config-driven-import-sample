8e0c604c1d9e5f53cedf474a695fbf6b177344e7
Junichi Hashimoto <46337314+JHashimoto0518@users.noreply.github.com>
2023-10-22 18:27:39 +0900

    enabled s3 versioning

--- single-import-sample/main.ts
+++ single-import-sample/main.ts
@@ -2,6 +2,7 @@ import { Construct } from "constructs";
 import { App, TerraformStack } from "cdktf";
 import { AwsProvider } from "@cdktf/provider-aws/lib/provider";
 import { S3Bucket } from "@cdktf/provider-aws/lib/s3-bucket";
+import { S3BucketVersioningA } from "@cdktf/provider-aws/lib/s3-bucket-versioning";
 
 
 class MyStack extends TerraformStack {
@@ -11,7 +12,14 @@ class MyStack extends TerraformStack {
       region: "ap-northeast-1",
     });
 
-    new S3Bucket(this, "bucket", {});
+    const bucket = new S3Bucket(this, "bucket", {});
+
+    new S3BucketVersioningA(this, "versioning", {
+      bucket: bucket.id,
+      versioningConfiguration: {
+        status: "Enabled",
+      }
+    })
   }
 }
 
